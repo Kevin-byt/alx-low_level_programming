9a6790dcaadf9381c3b22078b65ffae560bc97ac
Kevin-byt <myapps255@gmail.com>
2023-03-23 09:24:48 -0700

    leap year

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* main - Tests wher 65 is positive or negative
+* main - Test function for positive or negative
 * Return: 0
 */
 int main(void)
@@ -11,4 +11,3 @@ int main(void)
 	positive_or_negative(i);
 	return (0);
 }
-
