303446689d34cf59ca18024b8efa459d79b3c1e1
Kevin-byt <myapps255@gmail.com>
2023-04-17 13:18:39 -0700

    creates  struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic elements
+ * @name: First element (member)
+ * @age: Secondelement (member)
+ * @owner: Third element (member)
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
